4b04421a4d7aec38cc3deba25d413a170779ec52
Andrew Johnson <Andrew Johnson@10.10.16.148>
2021-07-07 08:00:25 -0600

    subtract method

--- GitTest/src/main/java/Main.java
+++ GitTest/src/main/java/Main.java
@@ -3,7 +3,15 @@ public class Main {
 
 	public static void main(String[] args) {
 		int x;
-
+		
+	}
+	
+	public int add(int x, int y) {
+		return x + y;
+	}
+	
+	public int subtract(int x, int y) {
+		return x - y;
 	}
 
 }
